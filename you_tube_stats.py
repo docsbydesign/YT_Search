43661dc258695b6d6d45d633937af1ddc1504cc1
Bob Watson <rbwatson@uw.edu>
2020-05-25 16:04:03 -0700

    Update you_tube_stats.py
    Added Video stats

--- you_tube_stats.py
+++ you_tube_stats.py
@@ -10,6 +10,7 @@ import sys
 import requests
 import json
 import csv
+import traceback
 from datetime import datetime
 #
 #   declare variables to access the YouTube API
@@ -18,51 +19,134 @@ you_tube_api_host = 'https://www.googleapis.com/youtube/v3/'
 you_tube_search_url = you_tube_api_host + 'search'
 you_tube_videos_url = you_tube_api_host + 'videos'
 
+def get_stats_for_video (video_id, video_stats_data):
+    #
+    # return statistics for specified video from buffer
+    #
+    for video in video_stats_data['items']:
+        if video['id'] == video_id:
+            return video['statistics']
 
-def print_search_csv (search_term,search_result):
+    return None
+
+
+def get_video_stats (api_key, search_results):
+    #
+    # Gets the video stats for the videos returned in the search results
+    #
+    video_ids = []
+    for item in search_results['items']:
+        video_ids.append(item['id']['videoId'])
+    #
+    # query YouTube for the data
+    #
+    query_data = {
+        'key': api_key,
+        'part': 'statistics',
+        'id': ','.join(video_ids)
+    }
+    #
+    query_headers = {
+        'Content-Type': 'application/json'
+    }
+    video_stats_data = None
+    try:
+        #
+        #   Get the data from YouTube
+        #
+        query_results = requests.get(you_tube_videos_url, params=query_data, headers=query_headers)
+        # if the request returned data, it should be a JSON string, so try to parse it
+        # check for a response instead of a successful status code to catch and display
+        # error responses as well as successful ones.
+        if query_results.text:
+            # parse the response
+            # if this doesn't work, the exception handler will catch it
+            video_stats_data = query_results.json()
+
+    except Exception as e:
+        # if an exception was raised, get the message
+        print(str(e))
+        print(traceback.format_exc())
+
+    return video_stats_data
+
+def print_search_csv (api_key, search_term, search_results):
     #
     # format selected fields as CSV and write to console
     #
     snippet_fields = [
-        "publishedAt",
         "title",
         "description",
         "channelTitle",
-        "channelId",
-        "publishTime"
+        "channelId"
     ]
+
+    stats_fields = [
+        "viewCount",
+        "likeCount",
+        "favoriteCount",
+        "commentCount"
+    ]
+
     csv_fields = [
         "searchTerm",
         "searchDate",
         "videoID",
-        "publishedAt",
+        "videoUrl",
+        "publishTime",
         "title",
         "description",
-        "video",
         "channelTitle",
         "channelId",
-        "publishTime"
+        "viewCount",
+        "likeCount",
+        "dislikeCount",
+        "favoriteCount",
+        "commentCount"
     ]
 
     time_now = datetime.now()
     # dd/mm/YY H:M:S
     search_time = time_now.strftime("%d/%m/%Y %H:%M:%S")
 
-    csvwriter = csv.writer(sys.stdout,
+    #
+    # get statistics for videos
+    #
+    video_stats_data = get_video_stats (api_key, search_results)
+
+    csv_writer = csv.writer(sys.stdout,
                             delimiter=',',
                             quotechar='"',
                             quoting=csv.QUOTE_MINIMAL)
-    csvwriter.writerow(csv_fields)
-    for item in search_result['items']:
-        csv_row = []
+    csv_writer.writerow(csv_fields)
+    for item in search_results['items']:
+        csv_row = []    #init the row list
+        #
+        #   load the values for the video
         csv_row = [
             search_term,
             search_time,
-            item['id']['videoId']
+            item['id']['videoId'],
+            'https://www.youtube.com/watch?v=' + item['id']['videoId'],
+            item['snippet']['publishedAt'].replace('T',' ').replace('Z','')
         ]
+        #
+        #   load the values from the search snippet object
         for field in snippet_fields:
             csv_row.append(item['snippet'][field].encode('utf_8',errors='ignore').decode('ascii',errors='ignore'))
-        csvwriter.writerow(csv_row)
+
+        #
+        #   load the values from the video's stats
+        video_stats = get_stats_for_video(item['id']['videoId'], video_stats_data)
+        for field in stats_fields:
+            try:
+                csv_row.append(str(video_stats[field]))
+            except:
+                # if the field is missing, assign a value of 0
+                csv_row.append("0")
+
+        #   write this video's row of data
+        csv_writer.writerow(csv_row)
 
 def search_you_tube(api_key, search_term):
     #
@@ -102,7 +186,7 @@ def search_you_tube(api_key, search_term):
             search_data = search_results.json()
             # format search data and print to console
             #print (json.dumps(search_data, indent = 4, separators = (',', ': ')))
-            print_search_csv(search_term, search_data)
+            print_search_csv(api_key, search_term, search_data)
             #
             #   Get search result count
             #
@@ -111,6 +195,7 @@ def search_you_tube(api_key, search_term):
     except Exception as e:
         # if an exception was raised, get the message
         print(str(e))
+        print(traceback.format_exc())
 
     return return_value
 
